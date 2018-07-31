38a6a02312a22f43f629a8b1dd1f152a554c8ced
bmisljen <bmisljen@gmail.com>
2018-07-31 10:47:37 -0400

    heroku 2

--- fluidestimatesnew/settings.py
+++ fluidestimatesnew/settings.py
@@ -36,7 +36,7 @@ def get_secret(setting, secrets=secrets):
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret('SECRET_KEY'),
+SECRET_KEY = config('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -97,7 +97,7 @@ EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'bmisljen@gmail.com'
-EMAIL_HOST_PASSWORD = get_secret('EMAIL_PASSWORD')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = 'bmisljen@gmail.com'
 
